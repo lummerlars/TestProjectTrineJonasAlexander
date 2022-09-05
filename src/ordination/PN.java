ff387c26e400029748ee5067dbcc14de365eac95
lummerlars <alexander-enoe@hotmail.com>
2022-09-05 10:31:05 +0200

    PN ændring

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -1,8 +1,9 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
-public class PN {
+public class PN extends Ordination {
     private double antalEnheder;
 
     /**
@@ -11,6 +12,11 @@ public class PN {
      * Returner false ellers, og datoen givesDen ignoreres.
      */
     public boolean givDosis(LocalDate givesDen) {
+        LocalDate startDate = super.getStartDen();
+        LocalDate slutDate = super.getSlutDen();
+        if (givesDen.isAfter(startDate) && givesDen.isBefore(slutDate)){
+            return true;
+        }
         return false;
     }
 
@@ -18,5 +24,21 @@ public class PN {
     public int getAntalGangeGivet() {
 
         return -1;
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        int daysBetween = 
+        return getAntalGangeGivet() * antalEnheder ;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
