40002947366f479d7d9ea2147eca701096f2fadd
trine <eaatrhc@students.eaaa.dk>
2022-09-05 11:03:23 +0200

    getType+doegndosis

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -5,11 +5,16 @@ int[] doser = new int[4];
 
     @Override
     public double samletDosis() {
+
         return 0;
     }
 
     @Override
     public double doegnDosis() {
+        int sum = 0;
+        for (int i = 0; i < doser.length; i++) {
+            sum += doser[i];
+        }
         return 0;
     }
 
