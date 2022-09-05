2b8dee6eca78a7b74a968afae0cb25b07df56522
trine <eaatrhc@students.eaaa.dk>
2022-09-05 09:45:09 +0200

    extended ordination

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,19 @@
 package ordination;
 
-public class DagligFast {
+public class DagligFast extends Ordination {
 
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
 }
