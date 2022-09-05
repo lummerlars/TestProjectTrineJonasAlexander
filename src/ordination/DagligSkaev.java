a25c3983706378b9aa83b4e61f85980e930c9837
jonas <jonas_wenneberg@hotmail.dk>
2022-09-05 09:48:59 +0200

    prøve

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -2,9 +2,25 @@ package ordination;
 
 import java.time.LocalTime;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination {
+
 
     public void opretDosis(LocalTime tid, double antal) {
 
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
