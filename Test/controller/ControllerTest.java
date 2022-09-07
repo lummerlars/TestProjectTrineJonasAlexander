035baa8a5c8bdde377c053f852ccbcaf2a0422c4
lummerlars <alexander-enoe@hotmail.com>
2022-09-07 12:52:54 +0200

    Projektet er næsten færdigt.

---
+++ Test/controller/ControllerTest.java
@@ -0,0 +1,48 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void opretPNOrdination() {
+    }
+
+    @Test
+    void opretDagligFastOrdination() {
+    }
+
+    @Test
+    void opretDagligSkaevOrdination() {
+    }
+
+    @Test
+    void ordinationPNAnvendt() {
+    }
+
+    @Test
+    void anbefaletDosisPrDoegn() {
+    }
+
+    @Test
+    void antalOrdinationerPrVaegtPrLaegemiddel() {
+    }
+
+    @Test
+    void getAllPatienter() {
+    }
+
+    @Test
+    void getAllLaegemidler() {
+    }
+
+    @Test
+    void opretPatient() {
+    }
+
+    @Test
+    void opretLaegemiddel() {
+    }
+}
